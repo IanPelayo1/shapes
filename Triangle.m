2dab7f30f6160434cd79fe419791351cadcbdd54
ianpelayo <ianpelayo@Mac.lan>
2026-05-15 22:10:17 -0700

    deleted the area fucntion code i couldnt get that method to work. Opted for the law of cosines since it simplifed finding the last side without assumtions.

--- Triangle.m
+++ Triangle.m
@@ -13,14 +13,10 @@ classdef Triangle
         end
 
         function a = area(obj)
-            h= sin(obj.a0) * obj.s2;
-            base1 = sqrt(obj.s2^2 - h^2);
-            area1= .5 * base1 * h;
-            area2= .5 * ( abs(obj.s1-base1))* h;
-            a = area1 + area2;
+            a= .5 * obj.s1 * obj.s2 *sin(obj.a0);
         end
         function p = perimeter(obj)
-            p = obj.s1 + obj.s2 + sqrt(obj.s2^2 - (sin(obj.a0) * obj.s2)^2);
+            p = obj.s1 + obj.s2 + sqrt((obj.s2^2 +obj.s1^2)-2*(obj.s2*obj.s1)*cos(obj.a0));
         end
     end 
 end
